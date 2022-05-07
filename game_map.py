aa880a66c4b47718c70b9686c3810cca30dc027b
Jason Morejon <jason.morejon@gmail.com>
2022-05-07 14:27:13 -0400

    fourth commit- enemies, though useless, spawn

--- game_map.py
+++ game_map.py
@@ -1,3 +1,7 @@
+from __future__ import annotations
+
+from typing import Iterable, TYPE_CHECKING
+
 import numpy as np
 from tcod import heightmap_add
 
@@ -5,10 +9,14 @@ from tcod.console import Console
 
 import tile_types
 
+if TYPE_CHECKING:
+    from entity import Entity
+
 
 class GameMap:
-    def __init__(self, width:int, height:int):
-        self.width, self.height = width, height
+    def __init__(self, width: int, height: int, entities: Iterable[Entity] = ()):
+        self.width, self.height = width, height 
+        self.entities = set(entities)
         self.tiles = np.full((width, height), fill_value=tile_types.wall, order="F")
 
         self.visible = np.full((width, height), fill_value=False, order="F")  # Tiles the player can currently see
@@ -24,3 +32,8 @@ class GameMap:
             choicelist=[self.tiles["light"], self.tiles["dark"]],
             default=tile_types.SHROUD
         )
+
+        for entity in self.entities:
+            # Only print entities that are in the FOV
+            if self.visible[entity.x, entity.y]:
+                console.print(x=entity.x, y=entity.y, string=entity.char, fg=entity.color)
